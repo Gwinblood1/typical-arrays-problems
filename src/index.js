1f217b9bce4f3a4b47f8418ed4d183a154e3bd64
Maksim Ivankou <fahrenheit9110@gmail.com>
2020-10-03 20:58:47 +0300

    init: start task Typical Array Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
-
 exports.min = function min (array) {
-  return 0;
+    let min = 0;
+    if  (!array || array.length === 0) {
+        return 0;
+      }
+    for (let i = 0; i < array.length; i++) {
+        if (min > array[i]) min = array[i];
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+     let max = 0;
+     if  (!array || array.length === 0) {
+        return 0;
+      }
+     
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) max = array[i];
+    } 
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    sum = 0;
+    if  (!array || array.length === 0) {
+        return 0;
+      }
+    
+    for (let i = 0; i < array.length; i++) {
+        sum = sum + array[i];
+    }
+    let avg = sum/array.length;
+  return avg;
 }
